2ef7042bc3602cb21ffcc49d8a8c67aec96df220
SohanR <mizan.rahman66d@gmail.com>
2020-06-25 17:40:52 +0600

    boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+# It's a multi page business website theme/HTML template.
+
+## What is Twidel ??? 
+### I named theme as Twidel,picked it randome and it has no meaning XD 
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  entry: ["react-hot-loader/patch", "./src/js/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+};
+
+module.exports = config;
