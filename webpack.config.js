1145ebb3c96414bb6cc890980b264b7a33172c3b
SohanR <mizan.rahman66d@gmail.com>
2020-06-26 20:54:35 +0600

    error fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+
 const webpack = require("webpack");
 const path = require("path");
 
 const config = {
-  entry: ["react-hot-loader/patch", "./src/js/index.js"],
+  entry: "./src/js/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
@@ -10,48 +11,10 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
-        exclude: /\.module\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
-          },
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.svg$/,
-        use: "file-loader",
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              mimetype: "image/png",
-            },
-          },
-        ],
       },
     ],
-  },
-  devServer: {
-    contentBase: "./dist",
   },
 };
 
